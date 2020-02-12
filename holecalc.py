c367a2ee7370b9bb539d50b839b92a4fd00bb212
rouyng <54250428+rouyng@users.noreply.github.com>
2020-02-11 18:28:16 -0700

    create basic hole calculator math

--- holecalc.py
+++ holecalc.py
@@ -1,2 +1,12 @@
+from math import sqrt
+
+
 def calculate_hole(pin1, pin2, pin3):
-    pass
+    def radius(dia):
+        return dia / 2
+
+    prod_of_rads = radius(pin1) * radius(pin2) * radius(pin3)
+    sum_of_rads = sum([radius(pin1), radius(pin2), radius(pin3)])
+    hole_dia = 2 * (prod_of_rads / (2 * sqrt(prod_of_rads * sum_of_rads) - (
+                (radius(pin1) * radius(pin2)) + (radius(pin2) * radius(pin3)) + (radius(pin1) * radius(pin3)))))
+    return hole_dia
