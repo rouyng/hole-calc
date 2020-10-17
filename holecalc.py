24cd43e6dc4072adfb3624011924fdcd6bd5e9f0
rouyng <54250428+rouyng@users.noreply.github.com>
2020-10-17 14:24:31 -0700

    add placeholders for flask templates. add placeholder function for calculating pin size.

--- holecalc.py
+++ holecalc.py
@@ -1,12 +1,20 @@
 from math import sqrt
 
 
-def calculate_hole(pin1, pin2, pin3):
+def calculate_hole_size(pin1: float, pin2: float, pin3: float):
+    """From three known pin f"""
     def radius(dia):
         return dia / 2
 
     prod_of_rads = radius(pin1) * radius(pin2) * radius(pin3)
     sum_of_rads = sum([radius(pin1), radius(pin2), radius(pin3)])
-    hole_dia = 2 * (prod_of_rads / (2 * sqrt(prod_of_rads * sum_of_rads) - (
-                (radius(pin1) * radius(pin2)) + (radius(pin2) * radius(pin3)) + (radius(pin1) * radius(pin3)))))
+    hole_dia = 2 * (prod_of_rads / (2 * sqrt(prod_of_rads * sum_of_rads) -
+                                    ((radius(pin1) * radius(pin2)) +
+                                     (radius(pin2) * radius(pin3)) +
+                                     (radius(pin1) * radius(pin3)))))
     return hole_dia
+
+
+def calculate_remaining_pin(hole_dia: float, pin1: float, pin2: float, ) -> float:
+    """From two known pin sizes, calculate the required third pin diameter to gauge hole diameter"""
+    pass
