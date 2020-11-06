e71011961400b951bf75d8d6d2a6d013fe7edc68
rouyng <54250428+rouyng@users.noreply.github.com>
2020-11-05 17:58:51 -0700

    update tests

--- tests/test_holecalc.py
+++ tests/test_holecalc.py
@@ -19,16 +19,18 @@ def test_import():
 class TestCalculations:
     """Tests mathematical accuracy of formulas"""
     def test_example_values_1(self):
-        assert round(holecalc.calculate_hole_size(1, 2, 3), 3) == 6
+        assert round(holecalc.calculate_hole_size(1, 2, 3)['result'], 3) == 6
 
     def test_example_values_2(self):
-        assert round(holecalc.calculate_hole_size(5, 2, 8), 3) == 24.375
+        assert round(holecalc.calculate_hole_size(5, 2, 8)['result'], 3) == 24.375
 
     def test_examples_values_3(self):
-        assert round(holecalc.calculate_hole_size(0.113, 0.278, 0.156), 3) == 0.440
+        assert round(holecalc.calculate_hole_size(0.113, 0.278, 0.156)['result'], 3) == 0.440
 
     def test_zero_input(self):
-        assert holecalc.calculate_hole_size(0, 1, 2) is None
+        assert holecalc.calculate_hole_size(0, 1, 2) == \
+               {'result': None, 'error': 'Pin dimension cannot be zero'}
 
     def test_invalid_input(self):
-        assert holecalc.calculate_hole_size(50, 1, 0.01) is None
+        assert holecalc.calculate_hole_size(50, 1, 0.01) == \
+               {'result': None, 'error': 'Cannot calculate hole dimension, check pin values'}
