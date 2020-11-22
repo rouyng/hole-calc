0c66c2283727dccb43db724c2ccd8c6ce633c739
rouyng <54250428+rouyng@users.noreply.github.com>
2020-11-22 16:50:30 -0700

    begin adding styling and js for toleranced mode

--- static/custom.js
+++ static/custom.js
@@ -1,12 +1,13 @@
 // Custom JS for hole-calc
 
-// function to toggle showing pin tolerance options
-const rdo = document.getElementById("tol-radio");
-    rdo.onclick = function () {
-    const fields = document.getElementsByClassName("tol-params");
-    if (rdo.checked) {
-      fields.style.visibility = "visible";
-    } else {
-     fields.style.display = "hidden";
+// function to toggle showing pin tolerance options.
+function toggleTol () {
+    let tolDivs = document.getElementsByClassName("tol-params")
+    for (let f of tolDivs) {
+            if (document.getElementById("tol").checked) {
+                f.style.visibility = "visible";
+            } else {
+                f.style.visibility = "hidden";
+            }
     }
 }
