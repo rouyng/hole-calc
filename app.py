a90789df603d40ae40c7ca2c6560be2e2436b29f
Ross Young <rssyoung@gmail.com>
2020-11-26 19:01:33 -0700

    refactor and create placeholders before redoing forms using flask-wtf/wtforms

--- app.py
+++ app.py
@@ -18,41 +18,34 @@ def guide():
     return render_template('guide.html')
 
 
-@app.route('/')
-def home():
-    placeholder_input = (None, None, None)
-    return render_template('3hole.html',
-                           valid=True,
-                           input_pins=placeholder_input,
-                           units="IN",
-                           precision="0.001", result="")
-
-
-@app.route('/', methods=['POST'])
-def post_results():
-    form_input_data = request.form
-    form_units = form_input_data['units']
-    precision = form_input_data['precision']
-    rounded_result = None
-    try:
-        pin1 = Decimal(form_input_data['pin1'])
-        pin2 = Decimal(form_input_data['pin2'])
-        pin3 = Decimal(form_input_data['pin3'])
-        input_validated = True
-    except InvalidOperation:
-        pin1, pin2, pin3 = None, None, None
-        input_validated = False
-    else:
-        calc_result = hc.calculate_hole_size(pin1, pin2, pin3)
+@app.route('/', methods=('GET', 'POST'))
+def home_calc():
+    if request.method == 'POST':
+        form_input_data = request.form
+        form_units = form_input_data['units']
+        precision = form_input_data['precision']
+        rounded_result = None
         try:
-            rounded_result = calc_result['result'].quantize(Decimal(form_input_data['precision']))
-            round_digits = len(precision) - 2
-            format_string = "{"+f":.{round_digits}f"+"}"
-            if 1 < len(precision) < 5:
-                rounded_result = format_string.format(round(calc_result['result'], round_digits))
-        except (TypeError, ValueError):
+            pin1 = Decimal(form_input_data['pin1'])
+            pin2 = Decimal(form_input_data['pin2'])
+            pin3 = Decimal(form_input_data['pin3'])
+            input_validated = True
+        except InvalidOperation:
+            pin1, pin2, pin3 = None, None, None
             input_validated = False
-            rounded_result = f"{calc_result['error']}"
+        else:
+            calc_result = hc.calculate_hole_size(pin1, pin2, pin3)
+            try:
+                rounded_result = calc_result['result'].quantize(Decimal(form_input_data['precision']))
+            except (TypeError, ValueError):
+                input_validated = False
+                rounded_result = f"{calc_result['error']}"
+    else:
+        form_units = 'IN'
+        pin1, pin2, pin3 = None, None, None
+        input_validated = True
+        precision = "0.001"
+        rounded_result = None
 
     return render_template('3hole.html',
                            valid=input_validated,
---
+++ forms.py
@@ -0,0 +1,3 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextField, SubmitField
+from wtforms.validators import DataRequired, Length
