4854d57413e60d3488fb0cc08c22180cd5af1bfb
Ross Young <rssyoung@gmail.com>
2020-12-01 23:07:40 -0700

    add gunicorn timeout parameter

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 source venv/bin/activate
-exec gunicorn -b :5000 -w 4 --access-logfile - --error-logfile - app:app
+exec gunicorn -b :5000 -w 4 --timeout 90 --access-logfile - --error-logfile - app:app
