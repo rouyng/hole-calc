dde1b7f51596d77ba7afece336c072b79ab8a11d
rouyng <54250428+rouyng@users.noreply.github.com>
2021-02-03 16:10:29 -0700

    add common error routes and error page templates

--- main.py
+++ main.py
@@ -210,6 +210,24 @@ def reverse_calc_render():
                            calc_menu=calc_menu)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    """Render 404 page not found template"""
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def page_not_found(e):
+    """Render 403 forbidden template"""
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    """Render 500 internal server error template"""
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
 
     app.run(debug=True)
