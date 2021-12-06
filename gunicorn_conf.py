58bb6216d8b1455015499cd31c4e827109cfddcf
rouyng <54250428+rouyng@users.noreply.github.com>
2021-12-06 10:40:52 -0700

    set worker_tmp_dir in gunicorn config

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,3 +1,4 @@
 # Configuration file for Gunicorn. Included because the base docker image was timing out workers
 workers = 4  # Define the number of processes to be opened for processing requests at the same time
-timeout = 2000
+timeout = 1000
+worker_tmp_dir = "/dev/shm"
