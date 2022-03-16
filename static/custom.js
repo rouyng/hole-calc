c11d36b56b76ee8d56067aa5a7a70cbcfe1afa22
rouyng <54250428+rouyng@users.noreply.github.com>
2022-03-16 09:03:07 -0700

    add script and form attributes to track form submit events

--- static/custom.js
+++ static/custom.js
@@ -3,19 +3,7 @@
 // run toggleTol when page loads, so if "toleranced" is selected the fields will show after reloading the page
 document.addEventListener("DOMContentLoaded", function () {
     toggleTol();
-    const form_analytics = document.querySelector("form[data-analytics]");
-    form_analytics.addEventListener('submit', registerAnalyticsEvent);
 });
-
-// add plausible() to record custom event goals in plausible.io analytics
-// see https://plausible.io/docs/custom-event-goals
-window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }
-
-
-
-function registerAnalyticsEvent(event) {
-    plausible(event.target.getAttribute('data-analytics'));
-}
 
 // function to toggle showing pin tolerance options.
 function toggleTol() {
@@ -29,6 +17,7 @@ function toggleTol() {
     }
 }
 
+// disable calculate button while loading result
 function loading() {
     let loaders = document.getElementsByClassName('loader');
     for (let i = 0; i < loaders.length; i++) {
@@ -37,3 +26,24 @@ function loading() {
     document.getElementById('calculate').disabled = true
 }
 
+// Plausible.io analytics event tracking scripts
+// add plausible() to record custom event goals in plausible.io analytics
+// see https://plausible.io/docs/custom-event-goals
+window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }
+
+// Send form submit events to plausible
+let buttons = document.querySelectorAll("button[data-analytics]");
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', handleFormEvent);
+  buttons[i].addEventListener('auxclick', handleFormEvent);
+}
+
+function handleFormEvent(event) {
+  event.preventDefault();
+  let attributes = event.target.getAttribute('data-analytics').split(/,(.+)/);
+  let events = [JSON.parse(attributes[0]), JSON.parse(attributes[1] || '{}')];
+  plausible(...events);
+  setTimeout(function () {
+      event.target.form.submit();
+  }, 150);
+}
